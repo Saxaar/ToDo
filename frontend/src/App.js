ec958cef2fcdf6c8d9e0e36f747af3401d148540
Aleksandrova Darya <57770001+shycoldii@users.noreply.github.com>
2021-07-27 12:53:29 +0300

    Готовая сборка

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component, useState, useEffect} from 'react';
+
+function App () {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch('/api/user/all')
+            .then(response => response.text())
+            .then(message => {
+                setMessage(message);
+            });
+    },[])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">{message}</h1>
+            </header>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    )
+}
+
+export default App;
